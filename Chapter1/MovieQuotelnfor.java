965cb6a1b2c968095c645886a88f2ab6207ccc1d
ShantalCabutaje <shantalcabutaje1@gmail.com>
2024-08-20 22:27:53 +0800

    Create MovieQuotelnfor.java

---
+++ Chapter1/MovieQuotelnfor.java
@@ -0,0 +1,10 @@
+public class MovieQuotelnfor{
+  public static void main (String[] args){
+
+    System.out.println("you can leave anytime");
+    System.out.println("movie: Fifty shades of grey");
+    System.out.println("Grey");
+    System.out.println("2015");
+
+  }    
+}
