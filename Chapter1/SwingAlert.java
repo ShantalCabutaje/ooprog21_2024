907af297d0847c6e53aeea7d3d970b0b438ec72f
ShantalCabutaje <shantalcabutaje1@gmail.com>
2024-08-22 14:24:31 +0800

    Create SwingAlert.java

---
+++ Chapter1/SwingAlert.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+public class HelloWorld{
+public static void main (String[] args) {
+// Display a simple dialog with the message "Hello World!"
+JOptionPane.showMessageDialog(null, "Hello, World!");
+}
+}
