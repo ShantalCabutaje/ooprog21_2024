4bc6d1e4b775ed433cbaff5d8013895cab47d1e4
ShantalCabutaje <shantalcabutaje1@gmail.com>
2024-09-20 20:03:24 +0800

    Create  FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,14 @@
+public class CompanyAddress {
+    public static void main(String[] args) {
+        String companyName = " ASUSTeK Computer Inc.";
+        String addressLine1 = "1501, Chung Ho Road, Sec. 3";
+        String addressLine2 = "Chung Ho City, New Taipei City, Taiwan";
+        String companyInfo = "Company: ASUS Inc.";
+
+        System.out.println("Output:");
+        System.out.println(companyName);
+        System.out.println(addressLine1);
+        System.out.println(addressLine2);
+        System.out.println(companyInfo);
+    }
+}
