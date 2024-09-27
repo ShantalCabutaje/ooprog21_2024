e9cfd1ceb04594d0d5405da1201ebc0477e09153
ShantalCabutaje <shantalcabutaje1@gmail.com>
2024-09-27 23:11:07 +0800

    Create task2:TestCircle.java

---
+++ Chapter 3/task2:TestCircle.java
@@ -0,0 +1,21 @@
+public class TestCircle {
+    public static void main(String[] args) {
+    
+        double radius1 = 3.0;
+        double diameter1 = 6.0;
+        double area1 = 28.274333882305138;
+
+        
+        double radius2 = 20.0;
+        double diameter2 = 40.0;
+        double area2 = 1256.6370614359173;
+
+        System.out.println("Radius of the first circle: " + radius1);
+        System.out.println("Diameter of the first circle: " + diameter1);
+        System.out.println("Area of the first circle: " + area1 + "\n");
+
+        System.out.println("Radius of the second circle: " + radius2);
+        System.out.println("Diameter of the second circle: " + diameter2);
+        System.out.println("Area of the second circle: " + area2);
+    }
+}
